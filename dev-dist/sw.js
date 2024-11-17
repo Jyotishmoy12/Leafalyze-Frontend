91244e771b57d8266fa60f2e330987d194604773
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-11-17 22:30:51 +0530

    some changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.k6cfn612998"
+    "revision": "0.s04st1sn2po"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
